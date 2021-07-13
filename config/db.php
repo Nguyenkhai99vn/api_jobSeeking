087edd380dd0c576780330df0aec6c4cd32d735b
Nguyenkhai99vn <72878036+Nguyenkhai99vn@users.noreply.github.com>
2021-07-14 02:46:14 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -10,7 +10,7 @@ class db{
     public function connect(){
         $this->conn = null;
         try {
-        $this->conn = new PDO("mysql:host=".$this ->servername.";port=3306;dbname=".$this->db."", $this->username, $this->password);
+        $this->conn = new PDO("mysql:host=".$this ->servername.";port=38338;dbname=".$this->db."", $this->username, $this->password);
         // set the PDO error mode to exception
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e) {
